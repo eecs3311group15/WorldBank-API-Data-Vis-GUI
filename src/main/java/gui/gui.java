ad58de3dcec1e610c1f32a656294f8cc875d2257
Kameel Anayi <94212382+KameelNK@users.noreply.github.com>
2022-10-20 14:47:54 -0400

    Add files via upload

---
+++ src/main/java/gui/gui.java
@@ -0,0 +1,112 @@
+package xdd;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+import com.csvreader.CsvReader;
+import com.csvreader.CsvWriter;
+
+class gui implements ActionListener{
+	
+	private static JLabel userName;
+	private static JLabel pass;
+	private static JButton button;
+	private static JTextField userT;
+	private static JPasswordField passT;
+	private static JFrame f;
+	private static ArrayList<String> emails = new ArrayList<String>();
+	private static ArrayList<String> passwords = new ArrayList<String>();
+	private static String path = "C:\\Users\\Kameel\\eclipse-workspace\\login\\src\\xdd\\user.csv";
+	
+	
+    public static void load(String path) throws Exception{
+    	
+        CsvReader reader = new CsvReader(path); 
+        reader.readHeaders();
+
+        while(reader.readRecord()){
+        	
+            emails.add(reader.get("email"));
+            passwords.add(reader.get("password"));
+        }
+       
+    }
+	
+    public static void main(String args[]) throws Exception{
+    	 
+    	f = new JFrame("Login");//creating instance of JFrame  
+    	JPanel panel = new JPanel();
+    	userT = new JTextField(10);
+    	passT = new JPasswordField(10);
+    	userName = new JLabel("Username:");
+    	pass = new JLabel("Password:");
+    	button = new JButton("Login");
+    	
+    	load(path);
+	   
+    	f.setSize(320, 145);
+    	f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    	f.setVisible(true);
+    	f.add(panel);
+   
+    	panel.setLayout(null);
+    	
+    	userName.setBounds(20, 15, 80, 20);
+    	panel.add(userName);
+    	
+    	pass.setBounds(20, 45, 80, 20);
+    	panel.add(pass);
+    	
+    	userT.setBounds(85, 17, 160, 20);
+    	panel.add(userT);
+    	
+    	passT.setBounds(85, 47, 160, 20);
+    	panel.add(passT);
+    	
+    	button.setBounds(100, 80, 80, 20);
+    	button.addActionListener(new gui());
+    	panel.add(button);
+        
+    }
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		String user = userT.getText();
+		String password = passT.getText();
+		int success = 0;
+		
+		for(int i = 0; i < emails.size(); i++) {
+			
+			
+			if (user.equals(emails.get(i)) && password.equals(passwords.get(i))) {
+			
+				success = 1;
+				
+			}	
+		}
+	
+		if (success == 1) {
+		
+			System.out.println(user + "  " + password);
+			System.out.println("Success");
+			f.setVisible(false);
+			success = 0;
+		
+		}
+		
+		else {
+			
+			System.out.println(user + "  " + password);
+			System.out.println("Failed");
+			
+		}
+	}
+}
