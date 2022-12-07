16c09113867d1044b3c706a49ceb94b0f4585b2d
Yihang(Derek) Yuan <derek80@my.yorku.ca>
2022-12-07 18:10:24 -0500

    Finish UC2 and UC7 Junit tests

--- src/test/java/analyses/UC7.java
+++ src/test/java/analyses/UC7.java
@@ -31,4 +31,43 @@ public class UC7 {
 		} 
 		assertEquals(expected, actual);
 	}
+	
+	@Test
+	public void ANN_PCT_CHG_Test() {
+		boolean expected = true;
+		boolean actual = false;
+		Analysis test = new ANN_PCT_CHG_co2EnergyPM25();
+		test.runAnalyses("CA", 2010, 2019);
+		double co2 = test.resultMap.get("2012").get(0);
+		if(co2 == -0.66088205886719) {
+			actual = true;
+		}		
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void Ratio_Test() {
+		boolean expected = true;
+		boolean actual = false;
+		Analysis test = new RATIO_co2GDP();
+		test.runAnalyses("CA", 2010, 2019);
+		double ratio = test.resultMap.get("2016").get(0);
+		if(ratio == 0.036441880447490534) {
+			actual = true;
+		}		
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void AVG_Test() {
+		boolean expected = true;
+		boolean actual = false;
+		Analysis test = new AVG_forestToLand();
+		test.runAnalyses("CA", 2010, 2019);
+		double ratio = test.resultMap.get("2019").get(0);
+		if(ratio == 38.71910270266652) {
+			actual = true;
+		}		
+		assertEquals(expected, actual);
+	}
 }
--- src/test/java/gui/UC2.java
+++ src/test/java/gui/UC2.java
@@ -2,6 +2,8 @@ package gui;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 import com.csvreader.CsvReader;
@@ -10,7 +12,7 @@ public class UC2 {
 	
 	
 	@Test
-	public void ExclusionListFileCheck() {
+	public void ExclusionListFileExist() {
 		boolean expected = true;
 		boolean actual = false;
 		String path = "excluded_countries.csv";
@@ -20,6 +22,21 @@ public class UC2 {
 			actual = reader.readHeaders();
 		} catch (Exception e) {
 			e.printStackTrace();
+		} 	
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void ExclusionListFileNotExist() {
+		boolean expected = false;
+		boolean actual = true;
+		String path = "123.csv1";
+		CsvReader reader;
+		try {
+			reader = new CsvReader(path);
+			actual = reader.readHeaders();
+		} catch (Exception e) {
+			actual = false;
 		} 	
 		assertEquals(expected, actual);
 	}
@@ -35,6 +52,27 @@ public class UC2 {
 	public void failExclusionListCheck() {
 		boolean expected = true;
 		boolean actual = AnalysisFacade.passExclusionCheck("Canada");
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void ListSizeCheck() {
+		boolean expected = true;
+		boolean actual = false;
+		String path = "excluded_countries.csv";
+		ArrayList<String> excludedCountries = new ArrayList<String>();	
+		CsvReader reader;
+		try {
+			reader = new CsvReader(path);
+			reader.readHeaders();
+		    while(reader.readRecord()){
+		    	excludedCountries.add(reader.get("name"));
+		    }
+		} catch (Exception e) {
+		} 	    	
+		if(excludedCountries.size() == 5) {
+			actual = true;
+		}	
 		assertEquals(expected, actual);
 	}
 }
